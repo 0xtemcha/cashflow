088491604f978f95c11b993b43c865b0116f0d20
temcha <itemchur@spin.ai>
2025-02-17 00:04:17 +0200

    added shadcn theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
 					<div>
 						<Link
 							href={transactionsPath()}
-							className={buttonVariants({ variant: 'outline' })}
+							className={buttonVariants({ variant: 'default' })}
 						>
 							My Transactions
 						</Link>
