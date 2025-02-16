5e05cb6aa767161fcdaf99f07be75b62af06e227
temcha <itemchur@spin.ai>
2025-02-16 17:33:54 +0200

    added transactions basic mocked data

---
+++ src/mocked-data.ts
@@ -0,0 +1,14 @@
+export const initialTransactions = [
+	{
+		id: '1',
+		title: 'Transaction 1',
+		description: 'Transaction 1 description',
+		type: 'INCOME',
+	},
+	{
+		id: '2',
+		title: 'Transaction 2',
+		description: 'Transaction 2 description',
+		type: 'OUTCOME',
+	},
+]
