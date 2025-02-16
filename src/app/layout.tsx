f06a4cbb0eb6f6c4877a0355d97d4df420b8ae65
temcha <itemchur@spin.ai>
2025-02-16 18:31:13 +0200

    added basic header with navigation links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Link from 'next/link'
+import { homePath, transactionsPath } from '@/paths'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -27,7 +29,21 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<nav className="bg-background/75 supports-backdrop-blur:bg-background/60 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
+					<div>
+						<Link href={homePath()} className="text-lg font-bold">
+							Home
+						</Link>
+					</div>
+					<div>
+						<Link href={transactionsPath()} className="text-lg font-bold">
+							My Transactions
+						</Link>
+					</div>
+				</nav>
+				<main className="bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-x-hidden overflow-y-auto px-8 py-24">
+					{children}
+				</main>
 			</body>
 		</html>
 	)
