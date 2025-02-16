7b93d56c96c88ae224998104002df8aecb4eb6ab
temcha <itemchur@spin.ai>
2025-02-17 00:43:32 +0200

    separated app header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css'
-import { LucideArrowRightLeft } from 'lucide-react'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import Link from 'next/link'
-import { buttonVariants } from '@/components/ui/button'
-import { homePath, transactionsPath } from '@/paths'
+import Header from '@/components/header'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -31,25 +28,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<nav className="bg-background/75 supports-backdrop-blur:bg-background/60 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
-					<div>
-						<Link
-							href={homePath()}
-							className={buttonVariants({ variant: 'ghost' })}
-						>
-							<LucideArrowRightLeft />
-							<h1 className="text-lg font-semibold">Cashflow</h1>
-						</Link>
-					</div>
-					<div>
-						<Link
-							href={transactionsPath()}
-							className={buttonVariants({ variant: 'default' })}
-						>
-							My Transactions
-						</Link>
-					</div>
-				</nav>
+				<Header />
 				<main className="bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-x-hidden overflow-y-auto px-8 py-24">
 					{children}
 				</main>
---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+import { LucideArrowRightLeft } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+import { homePath, transactionsPath } from '@/paths'
+import { buttonVariants } from './ui/button'
+
+const Header = () => {
+	return (
+		<nav className="bg-background/75 supports-backdrop-blur:bg-background/60 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
+			<div>
+				<Link
+					href={homePath()}
+					className={buttonVariants({ variant: 'ghost' })}
+				>
+					<LucideArrowRightLeft />
+					<h1 className="text-lg font-semibold">Cashflow</h1>
+				</Link>
+			</div>
+			<div>
+				<Link
+					href={transactionsPath()}
+					className={buttonVariants({ variant: 'default' })}
+				>
+					My Transactions
+				</Link>
+			</div>
+		</nav>
+	)
+}
+
+export default Header
