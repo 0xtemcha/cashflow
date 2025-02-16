4f95f1306bbbb7f0da6c1cc84aa8e39effcccdfb
temcha <itemchur@spin.ai>
2025-02-16 21:51:55 +0200

    added transaction type icons

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -2,13 +2,18 @@ import Link from 'next/link'
 import { initialTransactions } from '@/mocked-data'
 import { transactionPath } from '@/paths'
 
+const TRANSACTION_TYPE_ICONS = {
+	INCOME: '🤑',
+	OUTCOME: '💸',
+}
+
 const TransactionsPage = () => {
 	return (
 		<div>
 			{initialTransactions.map((transaction) => (
 				<div key={transaction.id}>
+					{TRANSACTION_TYPE_ICONS[transaction.type]}
 					<h2 className="text-lg">{transaction.title}</h2>
-
 					<Link
 						href={transactionPath(transaction.id)}
 						className="text-sm underline"
--- src/mocked-data.ts
+++ src/mocked-data.ts
@@ -3,12 +3,12 @@ export const initialTransactions = [
 		id: '1',
 		title: 'Transaction 1',
 		description: 'Transaction 1 description',
-		type: 'INCOME',
+		type: 'INCOME' as const,
 	},
 	{
 		id: '2',
 		title: 'Transaction 2',
 		description: 'Transaction 2 description',
-		type: 'OUTCOME',
+		type: 'OUTCOME' as const,
 	},
 ]
