d3c7f70a31a13e38841f1f21d85d9a38398f5eff
temcha <itemchur@spin.ai>
2025-02-17 23:45:51 +0200

    updated transaction item styles and markup

--- src/features/transaction/components/transaction-item.tsx
+++ src/features/transaction/components/transaction-item.tsx
@@ -1,36 +1,52 @@
+import { LucideArrowUpRightFromSquare } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 import { buttonVariants } from '@/components/ui/button'
 import { Card, CardContent, CardTitle } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
 import { transactionPath } from '@/paths'
 import { TRANSACTION_TYPE_ICONS } from '../constants'
 import type { Transaction } from '../types'
 
 type TransactionItemProps = {
 	transaction: Transaction
+	isDetail?: boolean
 }
 
-const TransactionItem = ({ transaction }: TransactionItemProps) => {
+const TransactionItem = ({ transaction, isDetail }: TransactionItemProps) => {
+	const detailButton = (
+		<Link
+			href={transactionPath(transaction.id)}
+			className={`text-sm ${buttonVariants({ variant: 'outline', size: 'icon' })}`}
+		>
+			<LucideArrowUpRightFromSquare />
+		</Link>
+	)
 	return (
-		<Card key={transaction.id} className="w-full max-w-[560px]">
-			<CardContent className="flex w-full items-center justify-between gap-x-6 p-4">
-				<div className="flex flex-col gap-1 overflow-x-hidden">
-					<CardTitle className="truncate">{transaction.title}</CardTitle>
-					<p className="text-muted-foreground line-clamp-1 text-sm">
-						{transaction.description}
-					</p>
-				</div>
-				<div className="flex items-center gap-4">
-					{TRANSACTION_TYPE_ICONS[transaction.type]}
-					<Link
-						href={transactionPath(transaction.id)}
-						className={`text-sm ${buttonVariants({ variant: 'outline' })}`}
-					>
-						View
-					</Link>
-				</div>
-			</CardContent>
-		</Card>
+		<div className="flex w-full max-w-[580px] gap-x-1">
+			<Card className="w-full">
+				<CardContent className="flex w-full items-start justify-between gap-x-6 p-4">
+					<div className="flex flex-col gap-1 overflow-x-hidden">
+						<CardTitle className="truncate">{transaction.title}</CardTitle>
+
+						<p
+							className={cn('text-muted-foreground text-sm', {
+								'line-clamp-1': !isDetail,
+							})}
+						>
+							{transaction.description}
+						</p>
+					</div>
+					<div className="flex items-center gap-4">
+						{TRANSACTION_TYPE_ICONS[transaction.type]}
+					</div>
+				</CardContent>
+			</Card>
+
+			{isDetail ? null : (
+				<div className="flex flex-col gap-y-1">{detailButton}</div>
+			)}
+		</div>
 	)
 }
 
