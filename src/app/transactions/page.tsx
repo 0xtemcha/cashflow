9d0a23fff83ea34f2c71ff3f51983d0924a92b20
temcha <itemchur@spin.ai>
2025-03-01 14:40:09 +0200

    static rendering vs dynamic rendering example

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -5,6 +5,9 @@ import { Placeholder } from '@/components/placeholder'
 import { Spinner } from '@/components/spinner'
 import { TransactionList } from '@/features/transaction/components/transaction-list'
 
+// Example of how to hop out of static rendering and into dynamic rendering
+// export const dynamic = 'force-dynamic'
+
 const TransactionsPage = async () => {
 	return (
 		<div className="flex flex-1 flex-col gap-y-8">
