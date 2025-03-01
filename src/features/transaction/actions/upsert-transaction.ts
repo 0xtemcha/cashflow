9bd981de1cd98c568fa5985869eabfa6b5270ec7
temcha <itemchur@spin.ai>
2025-03-01 19:52:54 +0200

    added general validation error on transaction upsert action

--- src/features/transaction/actions/upsert-transaction.ts
+++ src/features/transaction/actions/upsert-transaction.ts
@@ -2,26 +2,36 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
 import { transactionPath, transactionsPath } from '@/paths'
+
+const upsertTransactionSchema = z.object({
+	title: z.string().min(1).max(191),
+	description: z.string().min(1).max(1024),
+})
 
 export const upsertTransaction = async (
 	id: string | undefined,
 	_actionState: { message: string },
 	formData: FormData,
 ) => {
-	const data = {
-		title: formData.get('title') as string,
-		description: formData.get('description') as string,
-	}
+	try {
+		const data = upsertTransactionSchema.parse({
+			title: formData.get('title'),
+			description: formData.get('description'),
+		})
 
-	await prisma.transaction.upsert({
-		where: {
-			id: id || '',
-		},
-		update: data,
-		create: data,
-	})
+		await prisma.transaction.upsert({
+			where: {
+				id: id || '',
+			},
+			update: data,
+			create: data,
+		})
+	} catch (error) {
+		return { message: `Something went wrong. ${error}` }
+	}
 
 	revalidatePath(transactionsPath())
 	if (id) {
