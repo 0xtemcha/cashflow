ab0c793b76e0e665f24d130bb1a0e1db8c40e6db
temcha <itemchur@spin.ai>
2025-03-01 17:03:36 +0200

    example of upsert loading state using useTransaction

--- src/features/transaction/components/transaction-upsert-form.tsx
+++ src/features/transaction/components/transaction-upsert-form.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import type { Transaction } from '@prisma/client'
+import { LucideLoaderCircle } from 'lucide-react'
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -11,15 +14,27 @@ type TransactionUpsertFormProps = {
 }
 
 const TransactionUpsertForm = ({ transaction }: TransactionUpsertFormProps) => {
+	const [isPending, startTransition] = React.useTransition()
+
+	const upsertTransactionAction = (formData: FormData) => {
+		startTransition(async () => {
+			console.log(upsertTransaction.bind(null, transaction?.id))
+			await upsertTransaction.bind(null, transaction?.id)(formData)
+		})
+	}
+
 	return (
-		<form action={upsertTransaction.bind(null, transaction?.id)} className="flex flex-col gap-y-2">
+		<form action={upsertTransactionAction} className="flex flex-col gap-y-2">
 			<Label htmlFor="title">Title</Label>
 			<Input type="text" id="title" name="title" defaultValue={transaction?.title} />
 
 			<Label htmlFor="description">Description</Label>
 			<Textarea id="description" name="description" defaultValue={transaction?.description} />
 
-			<Button type="submit">{transaction ? 'Edit' : 'Add'}</Button>
+			<Button type="submit" disabled={isPending}>
+				{isPending && <LucideLoaderCircle className="animate-spin" />}
+				{transaction ? 'Edit' : 'Add'}
+			</Button>
 		</form>
 	)
 }
