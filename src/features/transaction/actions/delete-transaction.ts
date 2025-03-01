b0c1c1634f3ce86f791842eba73882c8c03ca4dd
temcha <itemchur@spin.ai>
2025-03-01 14:07:24 +0200

    redirect to transactions page after deleting a transaction

--- src/features/transaction/actions/delete-transaction.ts
+++ src/features/transaction/actions/delete-transaction.ts
@@ -1,6 +1,8 @@
 'use server'
 
+import { redirect } from 'next/navigation'
 import { prisma } from '@/lib/prisma'
+import { transactionsPath } from '@/paths'
 
 export const deleteTransaction = async (id: string) => {
 	await prisma.transaction.delete({
@@ -8,4 +10,6 @@ export const deleteTransaction = async (id: string) => {
 			id,
 		},
 	})
+
+	redirect(transactionsPath())
 }
