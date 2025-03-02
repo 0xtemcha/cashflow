9060f4e5b12b4d4adfe84c94d12da6f38fe05501
temcha <itemchur@spin.ai>
2025-03-02 18:01:07 +0200

    added transaction type (read/view)

--- src/features/transaction/components/transaction-more-menu.tsx
+++ src/features/transaction/components/transaction-more-menu.tsx
@@ -1,6 +1,15 @@
-import { Transaction } from '@prisma/client'
+import { Transaction, type TransactionType } from '@prisma/client'
 import { LucideTrash } from 'lucide-react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { TRANSACTION_STATUS_LABELS } from '../constants'
 
 type TransactionMoreMenuProps = {
 	transaction: Transaction
@@ -15,10 +24,22 @@ const TransactionMoreMenu = ({ transaction, trigger }: TransactionMoreMenuProps)
 		</DropdownMenuItem>
 	)
 
+	const transactionStatusRadioGroupItems = (
+		<DropdownMenuRadioGroup value={transaction.type}>
+			{(Object.keys(TRANSACTION_STATUS_LABELS) as Array<TransactionType>).map((key) => (
+				<DropdownMenuRadioItem key={key} value={key}>
+					{TRANSACTION_STATUS_LABELS[key]}
+				</DropdownMenuRadioItem>
+			))}
+		</DropdownMenuRadioGroup>
+	)
+
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
 			<DropdownMenuContent className="w-56" side="right">
+				{transactionStatusRadioGroupItems}
+				<DropdownMenuSeparator />
 				{deleteButton}
 			</DropdownMenuContent>
 		</DropdownMenu>
--- src/features/transaction/constants.tsx
+++ src/features/transaction/constants.tsx
@@ -4,3 +4,8 @@ export const TRANSACTION_TYPE_ICONS = {
 	INCOME: <LucidePlus color="var(--color-green-500)" />,
 	OUTCOME: <LucideMinus color="var(--color-destructive)" />,
 }
+
+export const TRANSACTION_STATUS_LABELS = {
+	INCOME: 'Income',
+	OUTCOME: 'Outcome',
+}
