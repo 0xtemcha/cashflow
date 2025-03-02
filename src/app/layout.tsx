f8a9203343344f93ec8d65a558fd0053df44e90d
temcha <itemchur@spin.ai>
2025-03-02 15:22:43 +0200

    added root template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Toaster } from 'sonner'
 import { Header } from '@/components/header'
-import { RedirectToast } from '@/components/redirect-toast'
 import { ThemeProvider } from '@/components/theme/theme-provider'
 
 const geistSans = Geist({
@@ -35,7 +34,6 @@ export default function RootLayout({
 						{children}
 					</main>
 					<Toaster expand={true} richColors={true} />
-					<RedirectToast />
 				</ThemeProvider>
 			</body>
 		</html>
